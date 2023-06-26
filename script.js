6b266f4837142ec81ad9c9b3f027653a36e647f1
KailanJr <kailanjr99@gmail.com>
2023-06-26 12:46:23 -0300

    big att

--- script.js
+++ script.js
@@ -2,24 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  //modo Condicional (If e Else)
-  //if (html.classList.contains("light")) {
-  //html.classList.remove("light")
-  //} else {
-  //html.classList.add("light")
-  //}
-
-  //modo toggle
-  //html.classList.toggle("light")
-
-  //pegar a tag img
+  // pegar a tag img
   const img = document.querySelector("#profile img")
-  //Substituir a img
+
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    // Se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/Avatar-light.png")
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    // ser tiver sem light mode, manter img atual
-    img.setAttribute("src", "./assets/Avatar.png")
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
